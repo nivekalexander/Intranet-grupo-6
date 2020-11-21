b92ae121947b1059316c834d6f76ae5e6eaf8283
unknown <nivekalexander.12@gmail.com>
2020-11-20 22:29:23 -0500

    kevin

--- view/login.php
+++ view/login.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
 
      
      <?php require_once('./frames/header.php');?>
 
 </head>
 <body>
-   <?php require_once('./frames/navbar.php');?>
-   <?php require_once('./frames/slidebar.php');?>
-   <?php require_once('./frames/footer.php');?>
+    <div id="fondoLogin">
+    <img id="fondoLogin" src="../assets/img/fondologin.svg" alt="fondo.svg">
+    <?php require_once('./frames/footer.php');?>
+    </div>
 </body>
 </html>
