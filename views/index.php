0ac81c0c591354433383b141876b14e89794e3ba
unknown <nivekalexander.12@gmail.com>
2020-11-22 03:00:13 -0500

    kevin

--- views/index.php
+++ views/index.php
@@ -1,14 +1,14 @@
 
-     
-   <?php require_once('./frames/header.php');?>
-   
-   <?php require_once('./frames/navbar.php');?>
-   
-   <?php require_once('./frames/slidebar.php');?>
-   
-   <?php require_once('main.php');?>
-   
-   <?php require_once('./frames/footer.php');?>   
+  
+<?php require_once('./frames/header.php');?>
+
+<?php require_once('./frames/navbar.php');?>
+
+<?php require_once('./frames/slidebar.php');?>
+
+<?php require_once('main.php');?>
+
+<?php require_once('./frames/footer.php');?>   
 
 
 
