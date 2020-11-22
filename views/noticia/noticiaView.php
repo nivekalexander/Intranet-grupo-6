794cac2bf2a934dbccdef10cc57a44f2400b896b
unknown <nivekalexander.12@gmail.com>
2020-11-21 20:33:02 -0500

    kevin

--- views/noticia/noticiaView.php
+++ views/noticia/noticiaView.php
@@ -1,3 +1,23 @@
+<style>
+/*Estilos Noticias*/
+.noti-tittle{
+    color: #F2994B;
+    font-weight: 700;
+}
+
+
+.btn-rounded{
+    background-color: #F2994B;
+    border-radius: 50rem !important;
+    color : #ffffff !important;
+}
+
+.espaciado{
+    padding: 40px;
+}
+
+/*Fin Estilos Noticias*/
+</style>
 <div>
   <div class="container espaciado">
       <h3 class="noti-tittle">Noticias</h3>
