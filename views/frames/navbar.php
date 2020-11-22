216094d38d3612464212be07649c6cdf5ce88ee1
Camilo Carabali <valanya39@gmail.com>
2020-11-21 22:58:08 -0500

    .

--- views/frames/navbar.php
+++ views/frames/navbar.php
@@ -81,16 +81,16 @@
  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+        <div class="modal-header Color-Slidebar">
+          <h5 class="modal-title dropdown-text-color" id="exampleModalLabel">¿Quieres cerrar sesión?</h5>
+          <button class="close dropdown-text-color" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-body">Presiona "Salir" para cerrar tu sesion.</div>
         <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="../login/loginView.php">Logout</a>
+          <button class="btn btn-secondary btn-gris" type="button" data-dismiss="modal">Cancelar</button>
+          <a class="btn btn-rounded btn-primary " href="../login/loginView.php">Salir</a>
         </div>
       </div>
     </div>
