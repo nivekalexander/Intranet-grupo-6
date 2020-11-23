1d0de3e759efcf184975ad4466ee2939dba3b359
Victor8069 <victor.zapata8069@gmail.com>
2020-11-23 00:39:54 -0500

    Eliminar imagen anuncio

--- views/noticia/noticiaSelect.php
+++ views/noticia/noticiaSelect.php
@@ -10,8 +10,8 @@
                 <img src="<?php echo $filas->not_url; ?>" class="card-img" alt="No se encontro">
             </div>
             <div class="card-footer text-muted">
-                <button class="btn btn-rounded" onclick="EditarNoticia(<?php echo $filas->not_id ?>);">Editar</button>
-                <button class="btn btn-rounded" onclick="BorrarNoticia(<?php echo $filas->not_id ?>);">Eliminar</button>
+                <button class="btn btn-rounded" onclick="EditarNoticia(<?php echo $filas->not_id; ?>);">Editar</button>
+                <button class="btn btn-rounded" onclick="BorrarNoticia('<?php echo $filas->not_id;?>' , '<?php echo $filas->not_url; ?>');">Eliminar</button>
             </div>
         </div>
     <?php endforeach; ?>
