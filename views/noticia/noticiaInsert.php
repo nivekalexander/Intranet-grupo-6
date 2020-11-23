73cacff77370d82dfdaadf555544d442d6f0b6e9
Victor8069 <victor.zapata8069@gmail.com>
2020-11-23 02:24:47 -0500

    Restriccion subir noticia

--- views/noticia/noticiaInsert.php
+++ views/noticia/noticiaInsert.php
@@ -24,7 +24,8 @@
 
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary btn-gris" data-dismiss="modal">Cerrar</button>
-        <button type="submit" id="subir-news" class="btn btn-primary btn-rounded" onclick="InsertNoticia();">Subir</button>
+        <button type="submit" id="subir-news" class="btn btn-primary btn-rounded" >Subir</button>
+        <!-- onclick="InsertNoticia();" -->
       </div>
       </form>  
     </div>
@@ -35,23 +36,24 @@
 <script>
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-
-          $('#noticiaModal').modal('hide');
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();            
+          }else{
+            InsertNoticia();
+            $('#noticiaModal').modal('hide');
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
 </script>
 
