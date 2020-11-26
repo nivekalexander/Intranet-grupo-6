8d393eacc55b694d459c94e8af946a1e22d63113
quihuang <quihuang2017@gmail.com>
2020-11-26 04:24:09 -0500

    xd

--- views/usuario/usuarioConfirm.php
+++ views/usuario/usuarioConfirm.php
@@ -2,7 +2,7 @@
 <div>
     <?php foreach ($this->rol->Select() as $filas): ?>
 
-        <span class="border border-warning" id="confirm" value="<?php echo $filas->rol_id;?>" onclick="ConfirmUsuario(<?php echo $filas->rol_id;?>);"></span>
+        <span class="border-warning" id="confirm" value="<?php echo $filas->rol_id;?>" onclick="ConfirmUsuario(<?php echo $filas->rol_id;?>);"></span>
         <label class="usuConfirm-text"><?php echo $filas->rol_nombre;?></label>
 
     <?php endforeach;?>
