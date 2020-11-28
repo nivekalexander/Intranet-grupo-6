6cacbc559b199336d850cf8438d0996479d49df7
Victor8069 <victor.zapata8069@gmail.com>
2020-11-28 00:15:05 -0500

    Solución main.php

--- views/main.php
+++ views/main.php
@@ -13,8 +13,7 @@
 	try{
 
 		
-		if (!ISSET($_REQUEST['ctrl']) )
-		{
+		if (!ISSET($_REQUEST['ctrl']) ){
 			require_once("error404.php");
 
 			require_once("error404.php");
@@ -23,51 +22,26 @@
 
 			$controller = strtolower($_REQUEST['ctrl']);
 			$accion = ucwords(strtolower(ISSET($_REQUEST['acti']) ? $_REQUEST['acti'] : 'Index'));
+			
 			if(!file_exists("../controllers/$controller.controller.php")){
 
 				$error="No Existe El Controlador";
 				require_once("error404.php");
 				
 			}else{
-
 				require_once("../controllers/$controller.controller.php");
 				$controller = ucwords($controller).'Controller';
 				$controller = new $controller;
 
-				if(method_exists($controller,$accion)){
-					
+				if(method_exists($controller,$accion)){					
 					call_user_func(array($controller,$accion));
-
 				}else{
-					require_once("../controllers/$controller.controller.php");
-					$controller = ucwords($controller).'Controller';
-					$controller = new $controller;
-
-					
-
-					if(method_exists($controller,$accion)){
-						
-						call_user_func(array($controller,$accion));
-						
-					}else{
-						$error="No Existe El Metodo";
-						require_once("error404.php");
-
-					}
-					
-				}
-
-					$error="No Existe El Metodo";
-					require_once("error404.php");
-
-				}
-				
-			}
-
-		
+					$error = "No Existe El Metodo";
+					require_once("error404.php");			
+				}				
+			}				
+		}		
 	}catch(Exception $e){
-
 		require_once("error404.php");
-		
 	}
 ?>
