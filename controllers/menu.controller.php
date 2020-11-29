283cc8a1e1332e2b1f30d31eae471f845fe46099
quihuang <quihuang2017@gmail.com>
2020-11-29 15:21:06 -0500

    ficha codigo

---
+++ controllers/menu.controller.php
@@ -0,0 +1,19 @@
+<?php  
+	require_once('../models/grupos.php');
+
+	class MenuController{
+
+		private $grupos;
+
+		function __Construct() 
+								{
+									$this->grupos=new Grupos();
+								}
+
+		public function Index()
+								{
+								
+                                }
+                                
+                            }
+ ?>
