ef74eea0fb5eb556c012db6233f5ef2c4225e7ae
quihuang <quihuang2017@gmail.com>
2020-11-30 04:15:10 -0500

    noticias no esta validando campo vacio

--- views/noticia/noticiaInsert.php
+++ views/noticia/noticiaInsert.php
@@ -13,59 +13,59 @@
 
       <div class="modal-body">     
 
-      <form id="formulario" name="formulario" class="needs-validation">   
-        <div class="custom-file btn rounded" style="background-color: #e3e6f0; overflow: hidden;">
-          <input type="number" name="idnews" id="id-news" hidden>
-          <input type="file" name="file-news" id="file-news" accept="image/*" lang="es" required>
-          <div class="invalid-feedback">No se a seleccionado ningun archivo</div>
-        </div>                        
-      </div>
+      <form id="formulario" name="formulario" class="needs-validation" novalidate>   
 
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary btn-gris" data-dismiss="modal" onclick="CancelarNoticia();">Cerrar</button>
-        <button type="button" id="btnguardar" class="btn btn-primary btn-rounded" >Subir</button>
-        <!-- onclick="InsertNoticia();" -->
-      </div>
-      </form>  
+          <div class="custom-file btn rounded" style="background-color: #e3e6f0; overflow: hidden;">
+               <input type="number" name="idnews" id="id-news" hidden>
+               <input type="file" name="file-news" id="file-news" accept="image/*" lang="es" required>
+               <div class="invalid-feedback">No se a seleccionado ningun archivo</div>                    
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary btn-gris" data-dismiss="modal" onclick="CancelarNoticia();">Cerrar</button>
+            <button type="button" id="btnguardar" class="btn btn-primary btn-rounded" >Subir</button>
+          </div>
+      </form>
+
+      <script>
+
+                // document.getElementById("customFileLang").innerHTML = document.getElementById("file-news").files[0].name;
+
+                // Example starter JavaScript for disabling form submissions if there are invalid fields
+                (function() {
+                    'use strict';        
+                    window.addEventListener('load', function() {         
+                      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                      var forms = document.getElementsByClassName('needs-validation');
+                      // Loop over them and prevent submission
+                      var validation = Array.prototype.filter.call(forms, function(form) {        
+                        document.getElementById("btnguardar").addEventListener('click', function(event) { 
+
+                          if (form.checkValidity() === true) {
+                            var nombreBoton = document.getElementById("btnguardar").innerHTML;
+                            if (nombreBoton == "Subir"){
+                              InsertNoticia();
+                              $('#noticiaModal').modal('hide');
+                            }
+                            if (nombreBoton == "Actualizar"){
+                              UpdateNoticia();
+                              $('#noticiaModal').modal('hide');
+                            }
+                          
+                          }
+                          if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();            
+                          }
+                          form.classList.add('was-validated');
+                        }, false);
+                      });
+                    }, false);
+                  })();
+                </script>             
     </div>
   </div>
 </div>
+
 <!--End Modal -->
-
-<script>
-
-// document.getElementById("customFileLang").innerHTML = document.getElementById("file-news").files[0].name;
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-    'use strict';        
-    window.addEventListener('load', function() {         
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {        
-        document.getElementById("btnguardar").addEventListener('click', function(event) { 
-          
-          if (form.checkValidity() === true) {
-            var nombreBoton = document.getElementById("btnguardar").innerHTML;
-            if (nombreBoton == "Subir"){
-              InsertNoticia();
-              $('#noticiaModal').modal('hide');
-            }
-            if (nombreBoton == "Actualizar"){
-              UpdateNoticia();
-              $('#noticiaModal').modal('hide');
-            }
-           
-          }
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();            
-          }
-          //form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
-</script>
 
