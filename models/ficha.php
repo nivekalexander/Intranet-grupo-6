d991c53492d7a5158c2315f828417056cb276f1a
Camilo Carabali <valanya39@gmail.com>
2020-12-01 01:06:56 -0500

    Actualizar

--- models/ficha.php
+++ models/ficha.php
@@ -36,7 +36,7 @@ class Ficha
 									 	try  				 {
 									 							$sql="INSERT INTO tbl_ficha (fic_codigo,fic_feccrn,fic_fecfn,fic_tijid,fic_modid,fic_tofid,fic_pfoid)
 									 										        VALUES(?,?,?,?,?,?,?);";
-									 							$this->pdo->prepare($sql)
+									 							if($this->pdo->prepare($sql)
 									 									  ->execute(
 									 									  			 array(
                                                                                                 $datos->fic_codigo,
@@ -47,7 +47,8 @@ class Ficha
                                                                                                 $datos->fic_tofid,
                                                                                                 $datos->fic_pfoid
 									 									  			 	   )
-									 									  			);
+																					   )){print('hola');}
+																					
 									 						 }
 									 	catch (Exception $e) {	die($e->getMessage());			 }
 									 }
