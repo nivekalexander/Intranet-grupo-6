6f205a3d6540a0cf1289db09ad19bc3471b3195a
unknown <nivekalexander.12@gmail.com>
2020-12-04 04:10:39 -0500

    grupos select

--- models/grupos.php
+++ models/grupos.php
@@ -11,20 +11,38 @@ class Grupos
 									 	catch (Exception $e) {	die($e->getMessage());			 }
 									 }
 
-		public function Select()
+		public function Select($id)
 									 {
 									 	try  				 {
 									 							$sql=$this->pdo->prepare("SELECT * FROM tbl_ficha 
-                                                                                          INNER JOIN tbl_programaformacion 
-                                                                                          INNER JOIN tbl_tipoprograma 
-                                                                                          WHERE tbl_ficha.fic_pfoid = tbl_programaformacion.pfo_id 
-                                                                                          and tbl_programaformacion.pfo_tprid = tbl_tipoprograma.tpr_id 
-                                                                                          ORDER BY tbl_ficha.fic_codigo DESC");
-									 							$sql->execute();
+																 							INNER JOIN tbl_programaformacion 
+																 							INNER JOIN tbl_tipoprograma
+																 							INNER JOIN  tbl_usuario_ficha  
+																 							WHERE tbl_ficha.fic_pfoid = tbl_programaformacion.pfo_id 
+																 							and tbl_programaformacion.pfo_tprid = tbl_tipoprograma.tpr_id 
+																 							and tbl_usuario_ficha.usf_usunumdnt = ?
+																 							and tbl_usuario_ficha.usf_ficcodigo =tbl_ficha.fic_codigo
+																 							ORDER BY tbl_ficha.fic_codigo DESC");
+									 							$sql->execute(array($id));
 									 							return $sql->fetchALL(PDO::FETCH_OBJ);
 									 						 }
 									 	catch (Exception $e) {	die($e->getMessage());			 }
 									 }
+		public function SelectAdmin()
+		{
+		try  				 {
+								$sql=$this->pdo->prepare("SELECT * FROM tbl_ficha 
+															INNER JOIN tbl_programaformacion 
+															INNER JOIN tbl_tipoprograma 
+															WHERE tbl_ficha.fic_pfoid = tbl_programaformacion.pfo_id 
+															and tbl_programaformacion.pfo_tprid = tbl_tipoprograma.tpr_id 
+															ORDER BY tbl_ficha.fic_codigo DESC");
+								$sql->execute();
+								return $sql->fetchALL(PDO::FETCH_OBJ);
+								}
+		catch (Exception $e) {	die($e->getMessage());			 }
+		}
+
 					 
 
 	}
--- views/grupos/gruposSelect.php
+++ views/grupos/gruposSelect.php
@@ -3,9 +3,14 @@
 
 <?php $_SESSION['slidebar']=$_REQUEST['slidebar'];?>
 
-    <?php foreach ( $this->grupos->Select() as $filas ): 
+    <?php 
+    
+      if($_SESSION['SRol']!=1){
+        foreach ( $this->grupos->Select($_SESSION['SIdu']) as $filas ): 
+        
+        
       
-      if($filas->fic_codigo != "000000"){
+
 
       ?>
 
@@ -44,8 +49,59 @@
 
       </form>
 
-    <?php }
-  
-    endforeach; ?>
+    <?php 
+    
+    endforeach; } 
+    else{
+      foreach ( $this->grupos->SelectAdmin() as $filas ): 
+      
+      
+    
 
-</div> 
+
+    ?>
+
+
+    <form method="POST" action="main.php?ctrl=noticia">
+
+    <div class="col mb-4 card-deck">  
+    
+      <div class="card bg-light mb-3 ">
+
+        <div class="card-header font-weight-bold">
+          <div class="float-left noti-tittle" style="font-size: 25px;">
+           <?php echo $filas->fic_codigo;?>
+          </div>            
+          <div class="float-right">
+            <a  name="imagenPost"><img src="../assets/img/logosena.svg" width="40" height="40"></a>
+          </div>
+        </div>
+
+        <div class="card-body">
+          <h6 class="card-title font-weight-bold">Programa: </h6>
+          <p class="card-text"><?php echo $filas->pfo_nompro;?></p>
+          <h6 class="card-title font-weight-bold">Tipo: </h6>
+          <p class="card-text"><?php echo $filas->tpr_nombre;?></p>
+        </div>
+
+        <div class="card-footer">
+            <input type="text" name="fichapuntero" id="fichapuntero" value="<?php echo $filas->fic_codigo; ?>" hidden> 
+            <input type="text" name="slidebar" id="slidebar" value="1" hidden>
+            <button class="btn-rounded btn float-right" type="submit" >Entrar</button>
+          
+          </div>
+
+      </div>
+    </div>
+
+    </form>
+
+  <?php 
+  
+  endforeach; }?>
+
+
+    
+  
+
+
