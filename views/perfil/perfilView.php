bba27bdd17bc83703b343520fca853b6b2959d13
quihuang <quihuang2017@gmail.com>
2020-12-07 01:38:39 -0500

    validaciones del modulo de perfil

--- views/perfil/perfilView.php
+++ views/perfil/perfilView.php
@@ -2,10 +2,11 @@
 <script src="../assets/js/md5/md5.js"></script>
 <script src="../assets/js/scripts.perfil.js"></script>
 <div class="container espaciado">
+    <a class="float-right btn-rounded btn" <?php echo $_SESSION['SRol'] !=3 ? 'href="main.php?ctrl=grupos"':'href="main.php?ctrl=noticia"'; ?>>Volver</a>
     <h3 class="noti-tittle">Perfil</h3>
     <div class="border border-secondary panel-perfil" readonly>
         <div class="panel-foto rounded-circle">
-            <img class="img-profile" src="https://www.flaticon.es/svg/static/icons/svg/599/599305.svg" alt="">
+            <img class="img-profile" src="../assets/img/img-perfil/user.svg" alt="">
         </div>
         <div id="tview" class="container panel-datos rounded">
             <?php include('perfilForm.php');?>
